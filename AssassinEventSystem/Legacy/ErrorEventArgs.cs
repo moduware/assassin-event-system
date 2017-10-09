37152abf6f347eb1aa52971f7803392724e432d5
Moemen Hegazy <moemen.hegazy@gmail.com>
2017-10-09 17:05:21 +1100

    added 2 legacy classes from common types assembly

---
+++ AssassinEventSystem/Legacy/ErrorEventArgs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AssassinEventSystem.Legacy
+{
+    public class ErrorEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The exception.
+        /// </summary>
+        public Exception Exception;
+
+        /// <summary>
+        /// The message.
+        /// </summary>
+        public string Message;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Nexpaq.EventArguments.ErrorEventArgs"/> class.
+        /// </summary>
+        /// <param name="e">E.</param>
+        /// <param name="m">M.</param>
+        public ErrorEventArgs(Exception e, string m)
+        {
+            Exception = e;
+            Message = m;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Nexpaq.EventArguments.ErrorEventArgs"/> class.
+        /// </summary>
+        /// <param name="m">M.</param>
+        public ErrorEventArgs(string m)
+        {
+            Message = m;
+            Exception = new Exception(m);
+        }
+    }
+}
---
+++ AssassinEventSystem/Legacy/ErrorSystemClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AssassinEventSystem.Legacy
+{
+    /// <summary>
+    /// Class with support of error reporting system 
+    /// (for comman errors, not exceptions)
+    /// </summary>
+    public class ErrorSystemClass
+    {
+        #region METHODS
+        /// <summary>
+        /// Triggers the error.
+        /// </summary>
+        /// <param name="message">Message of error.</param>
+        protected void TriggerError(string message)
+        {
+            Error(this, new ErrorEventArgs(message));
+        }
+        #endregion
+
+        #region EVENTS
+        /// <summary>
+        /// Occurs when error.
+        /// </summary>
+        public event EventHandler<ErrorEventArgs> Error = delegate { };
+        #endregion
+    }
+}
